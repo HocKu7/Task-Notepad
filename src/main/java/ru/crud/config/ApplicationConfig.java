8122cf40409d1f0727e57697515500a10b5e7bf6
Ruslan Musaev <ruslan_musaev@epam.com>
2020-04-07 00:24:13 +0300

    init hello window

---
+++ src/main/java/ru/crud/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package ru.crud.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.crud")
+public class ApplicationConfig {
+
+}
---
+++ src/main/java/ru/crud/config/MainWebApplicationInitializer.java
@@ -0,0 +1,26 @@
+package ru.crud.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class MainWebApplicationInitializer implements WebApplicationInitializer {
+
+  @Override
+  public void onStartup(ServletContext servletContext) throws ServletException {
+
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(ApplicationConfig.class);
+
+    servletContext.addListener(new ContextLoaderListener(context));
+    ServletRegistration.Dynamic appServlet = servletContext.addServlet("mvc", new DispatcherServlet(
+        context));
+    appServlet.setLoadOnStartup(1);
+    appServlet.addMapping("/");
+  }
+}
