8f226b0c76cacfb8ab6ed605548cb4043e477d73
Ruslan Musaev <ruslan_musaev@epam.com>
2020-04-07 10:43:58 +0300

    add Swagger

--- src/main/java/ru/crud/config/WebConfig.java
+++ src/main/java/ru/crud/config/WebConfig.java
@@ -2,10 +2,19 @@ package ru.crud.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
 }
--- src/main/java/ru/crud/controller/UserController.java
+++ src/main/java/ru/crud/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.crud.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class UserController {
 
   @GetMapping("/hello")
   public String welcome() {
+    return "welcome";
+  }
+
+  @PostMapping("/hello")
+  public String welcomePost() {
     return "welcome";
   }
 
